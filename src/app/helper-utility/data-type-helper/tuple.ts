17d397aabcd510b756c022c3371ec8d50859815a
sunny <sunny365.25days@gmail.com>
2026-02-06 22:37:29 +0900

    fix: co-wrk

--- src/app/helper-utility/data-type-helper/tuple.ts
+++ src/app/helper-utility/data-type-helper/tuple.ts
@@ -17,12 +17,3 @@ export class Tuple3<T1, T2, T3> {
   ) {}
 }
 
-//要素4 Tuple
-export class Tuple4<T1, T2, T3, T4> {
-  constructor(
-    public item1: T1,
-    public item2: T2,
-    public item3: T3,
-    public item4: T4,
-  ) {}
-}
